b0271539d67589e50d2cc38eb438cbce5cf152d5
dcake19 <dcakebread82@gmail.com>
2023-08-05 10:34:45 +0100

    update view model tests

--- movies-ui/src/test/java/com/example/moveis_ui/DetailsViewModelTest.kt
+++ movies-ui/src/test/java/com/example/moveis_ui/DetailsViewModelTest.kt
@@ -1,10 +1,16 @@
 package com.example.moveis_ui
 
 import androidx.lifecycle.SavedStateHandle
+import com.example.moveis_ui.details.CastState
+import com.example.moveis_ui.details.CrewState
 import com.example.moveis_ui.details.DetailsState
 import com.example.moveis_ui.details.DetailsViewModel
 import com.example.movie_domain.details.GetMoviesDetailsUseCase
 import com.example.movie_domain.details.MoviesDetailsEntity
+import com.example.movie_domain.people.CastEntity
+import com.example.movie_domain.people.CreditsEntity
+import com.example.movie_domain.people.CrewEntity
+import com.example.movie_domain.people.GetCreditsUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.resetMain
@@ -13,9 +19,7 @@ import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito
 import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.doThrow
 import org.mockito.kotlin.mock
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -37,6 +41,25 @@ class DetailsViewModelTest {
         "This is the test overview."
     )
 
+    private val cast = (1..3).map {
+        CastEntity(
+            it,
+            "Name $it",
+            "Character $it",
+            "profile_path_$it"
+        )
+    }
+    private val crew = (1..3).map {
+        CrewEntity(
+            it,
+            "Name $it",
+            "Job $it",
+            "profile_path_$it"
+        )
+    }
+
+    private val creditsEntity = CreditsEntity(cast, crew)
+
     @After
     fun tearDown() {
         Dispatchers.resetMain()
@@ -54,13 +77,19 @@ class DetailsViewModelTest {
         val ssh = SavedStateHandle()
         ssh["movieId"] = id.toString()
 
-        val useCase = mock<GetMoviesDetailsUseCase> {
+        val detailsUseCase = mock<GetMoviesDetailsUseCase> {
             onBlocking { invoke(id) } doReturn moviesDetailsEntity
         }
 
-        val viewModel = DetailsViewModel(ssh, useCase)
+        val creditsUseCase = mock<GetCreditsUseCase> {
+            onBlocking { invoke(id) } doReturn creditsEntity
+        }
 
-         assert(viewModel.detailsFlow.value == DetailsState.Ready(moviesDetailsEntity))
+        val viewModel = DetailsViewModel(ssh, detailsUseCase, creditsUseCase)
+
+        assert(viewModel.detailsFlow.value == DetailsState.Ready(moviesDetailsEntity))
+        assert(viewModel.castFlow.value == CastState.Ready(cast))
+        assert(viewModel.crewFlow.value == CrewState.Ready(crew))
     }
 
     @Test
@@ -70,19 +99,17 @@ class DetailsViewModelTest {
         val ssh = SavedStateHandle()
         ssh["movieId"] = id.toString()
 
-       val useCase = mock<GetMoviesDetailsUseCase>{
-            onBlocking { invoke(id) }.thenThrow() //doThrow(Exception())
+        val creditsUseCase = mock<GetCreditsUseCase> {
+            onBlocking { invoke(id) } doReturn creditsEntity
         }
 
-//        Mockito
-//            .`when`(useCase.invoke(id))
-//            .thenThrow()
-
-        val viewModel = DetailsViewModel(ssh, useCase)
+       val detailsUseCase = mock<GetMoviesDetailsUseCase> {
+           onBlocking { invoke(id) }.thenThrow() //doThrow(Exception())
+        }
+        
+        val viewModel = DetailsViewModel(ssh, detailsUseCase, creditsUseCase)
 
         assert(viewModel.detailsFlow.value == DetailsState.Error)
-
-
     }
 
 }
